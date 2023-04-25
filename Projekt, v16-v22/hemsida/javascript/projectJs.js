a9eb047832d1083dfeccb4c9d81933f8c8d8a761
W1LST3R <131661191+W1LST3R@users.noreply.github.com>
2023-04-25 11:05:50 +0200

    Update projectJs.js

--- Projekt, v16-v22/hemsida/javascript/projectJs.js
+++ Projekt, v16-v22/hemsida/javascript/projectJs.js
@@ -1,17 +1,22 @@
 require(["esri/map","esri/layers/Graphic", "esri/InfoTemp","esri/geometry/Point",
 "esri/symbols/PictureMarkerSymbol","esri/graphic","esri/Color","dojo/domReady!"],
-  function(Map, GraphicsLayer){
-    map = new Map("mapDiv",{
-      basemap: "streets",
-      center: [60.208251508,17.923597762],
-      zoom: 14
-    });
+	function(Map, GraphicsLayer){
+		map = new Map("mapDiv",{
+      			basemap: "streets",
+      			center: [60.208251508,17.923597762],
+      			zoom: 14
+    		});
+	
+	var graphics = new GraphicsLayer();
+	map.addLayer(graphics);
+	}
+};
+	
+function getPoiData(){
+	var poiData = {url:"
+}
 
-  
-  
-  
-  function makePoi() {
+function makePoi() {
 	var poiLayer = new esri.layers.GraphicsLayer();
 	map.addLayer(poiLayer);
-
 }

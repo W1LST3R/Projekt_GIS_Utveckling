b8267f9147a604ec0dc85424f7f6a6d9a695e231
W1LST3R <131661191+W1LST3R@users.noreply.github.com>
2023-04-25 10:40:11 +0200

    Update projectJs.js

---
+++ Projekt, v16-v22/hemsida/javascript/projectJs.js
@@ -0,0 +1,8 @@
+require(["esri/map","esri/layers/Graphic", "esri/InfoTemp","esri/geometry/Point",
+"esri/symbols/PictureMarkerSymbol","esri/graphic","esri/Color","dojo/domReady!"],
+  function(Map, GraphicsLayer){
+    map = new Map("mapDiv",{
+      basemap: "streets",
+      center: [],
+      zoom: 14
+    });
